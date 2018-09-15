5d43804b6f8bb05cfb12b7687e1547d72e6f4f00
wagner <wagner@jcstarcon.com.br>
2018-09-14 23:26:33 -0300

    Ajustes para recuperar perguntas do Bot Ignorar respostas dos outros oraculos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using StackExchange.Redis;
+﻿using Google.Apis.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,30 +29,56 @@ namespace Oraculo
             // Notify subscriber(s) if you're joining
             pubsub.Publish(ChatChannel, $": {oraculo_group} entrou esta participando do grupo de oraculos.");
 
+            //PesquisaGoogle();
             // Messaging here
             while (true)
             {
-                pubsub.Publish(ChatChannel, $"{oraculo_group}: {Console.ReadLine()}");
+                var resposta = Console.ReadLine();
+                if (resposta != "")
+                {
+                    pubsub.Publish(ChatChannel, $"{oraculo_group}: {resposta}");  
+                }
+                
             }
         }
 
         static void MessageAction(string message)
         {
-            int initialCursorTop = Console.CursorTop;
-            int initialCursorLeft = Console.CursorLeft;
+            var teste = message.Split(':');
 
-            Console.MoveBufferArea(0, initialCursorTop, Console.WindowWidth,
-                1, 0, initialCursorTop + 1);
-            Console.CursorTop = initialCursorTop;
-            Console.CursorLeft = 0;
+            if (teste.Count() == 2)
+            {
+                #region Controle de cursor
+                int initialCursorTop = Console.CursorTop;
+                int initialCursorLeft = Console.CursorLeft;
 
-            // Print the message here
-            var PerguntaNumero = message.ToString().Substring(0, message.ToString().IndexOf(":"));
-            var Pergunta = message.ToString().Substring(message.ToString().IndexOf(":") + 1); ;
-            Console.WriteLine(Pergunta);
+                Console.MoveBufferArea(0, initialCursorTop, Console.WindowWidth,
+                    1, 0, initialCursorTop + 1);
+                Console.CursorTop = initialCursorTop;
+                Console.CursorLeft = 0;
+                #endregion
+                var PerguntaNumero_GroupName = teste[0];
+                var Pergunta_Resposta = teste[1];
 
-            Console.CursorTop = initialCursorTop + 1;
-            Console.CursorLeft = initialCursorLeft;
+                if (PerguntaNumero_GroupName != oraculo_group)
+                {
+                    if(PerguntaNumero_GroupName != "" && PerguntaNumero_GroupName.Substring(0,1).Equals("P"))
+                    {
+                        Console.WriteLine($"*****************************{PerguntaNumero_GroupName}*****************************************");
+                        Console.WriteLine(Pergunta_Resposta);
+                        Console.WriteLine("************************************************************************");
+                        Console.WriteLine("Resposta: ");
+                        Console.CursorTop = initialCursorTop + 3;                        
+                        Console.CursorLeft = 10;
+                    }
+                    else
+                    {
+                        Console.WriteLine(Pergunta_Resposta);
+                        Console.CursorTop = initialCursorTop + 3;
+                        Console.CursorLeft = 0;
+                    }                                        
+                }
+            }
         }
     }
 }
