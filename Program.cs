8c0194c8a628365800f5d10e79a7b523eeca69a4
Wagner <wsinfo@msn.com>
2018-09-19 21:06:28 -0300

    comunicação com o server atraves do hset

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Oraculo
 {
     class Program
     {
-        private const string RedisConnectionString = "127.0.0.1:6379";
+        private static string chave;
+        private const string RedisConnectionString = "191.232.234.20";
         private static ConnectionMultiplexer connection;
 
         private const string ChatChannel = "perguntas";
@@ -18,26 +19,30 @@ namespace Oraculo
         static void Main()
         {
             connection = ConnectionMultiplexer.Connect(RedisConnectionString);
-            
-            oraculo_group = "wagner_group";
+
+            oraculo_group = "Wagner_DanielSchuck";
 
             var pubsub = connection.GetSubscriber();
 
+            var perguntaId = "";
             pubsub.Subscribe(ChatChannel, (channel, message) => MessageAction(message));
 
-            pubsub.Publish(ChatChannel, $": {oraculo_group} esta participando do grupo de oraculos.");
+            //pubsub.Publish(ChatChannel, $": {oraculo_group} esta participando do grupo de oraculos.");
 
+            var db = connection.GetDatabase();
             while (true)
             {
                 var resposta = Console.ReadLine();
                 if (resposta != "")
                 {
-                    pubsub.Publish(ChatChannel, $"{oraculo_group}: {resposta}");  
+                    //pubsub.Publish(ChatChannel, $"{oraculo_group}: {resposta}");  
+                    db.HashSet(chave, oraculo_group, resposta);
                 }
                 
             }
         }
 
+        
         static void MessageAction(string message)
         {
             var teste = message.Split(':');
@@ -56,18 +61,32 @@ namespace Oraculo
 
                 var PerguntaNumero_GroupName = teste[0];
                 var Pergunta_Resposta = teste[1];
+                chave = PerguntaNumero_GroupName;
+                //var somaSplit = Pergunta_Resposta.Replace("?", "").Split('+');
+                //var s1 = Convert.ToInt16(somaSplit[0]);
+                //var s2 = Convert.ToInt16(somaSplit[1]);
+                //var resposta = s1+s2;
+                // var resposta = Console.ReadLine();// "Rgms presentes 330029,330222";
+
+                //Console.WriteLine("Respondido");
 
                 if (PerguntaNumero_GroupName != oraculo_group)
                 {
-                    if(PerguntaNumero_GroupName != "" && PerguntaNumero_GroupName.Substring(0,1).Equals("P"))
+
+                    if (PerguntaNumero_GroupName != "" && PerguntaNumero_GroupName.Substring(0, 1).Equals("P"))
                     {
                         #region Tratamento da pergunta que virá do bot
                         Console.WriteLine($"*****************************{PerguntaNumero_GroupName}*****************************************");
                         Console.WriteLine(Pergunta_Resposta);
-                        Console.WriteLine("************************************************************************");
-                        Console.WriteLine("Resposta: ");
-                        Console.CursorTop = initialCursorTop + 3;                        
+                        //Console.WriteLine("************************************************************************");
+                        //Console.WriteLine("Resposta: ");
+                        Console.CursorTop = initialCursorTop + 3;
                         Console.CursorLeft = 10;
+
+                        //var resposta = Console.ReadLine();
+                        //db.HashSet(PerguntaNumero_GroupName, oraculo_group, resposta, When.NotExists);
+                        //Console.WriteLine("Respondido");
+
                         #endregion
                     }
                     else
@@ -77,7 +96,8 @@ namespace Oraculo
                         Console.CursorTop = initialCursorTop + 3;
                         Console.CursorLeft = 0;
                         #endregion
-                    }                                        
+                    }
+
                 }
             }
         }
